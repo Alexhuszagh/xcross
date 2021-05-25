790cd1496d6a372bc9d2edbe456f03a1426a07b9
Alex Huszagh <ahuszagh@gmail.com>
2021-05-24 19:46:05 -0500

    Use bash arrays.

--- push.sh
+++ push.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-docker push ahuszagh/cross:base
-docker push ahuszagh/cross:alpha
-docker push ahuszagh/cross:arm
-docker push ahuszagh/cross:armhf
-docker push ahuszagh/cross:arm64
-docker push ahuszagh/cross:hppa
-docker push ahuszagh/cross:i686
-docker push ahuszagh/cross:m68k
-docker push ahuszagh/cross:mips
-docker push ahuszagh/cross:mips64
-docker push ahuszagh/cross:mips64el
-docker push ahuszagh/cross:mips64r6
-docker push ahuszagh/cross:mips64r6el
-docker push ahuszagh/cross:mipsel
-docker push ahuszagh/cross:mipsr6
-docker push ahuszagh/cross:mipsr6el
-docker push ahuszagh/cross:ppc
-docker push ahuszagh/cross:ppc64
-docker push ahuszagh/cross:ppc64le
-docker push ahuszagh/cross:riscv64
-docker push ahuszagh/cross:s390x
-docker push ahuszagh/cross:sh4
-docker push ahuszagh/cross:sparc64
-docker push ahuszagh/cross:x86_64
+DOCKERFILES=(
+    "base"
+    "alpha"
+    "arm"
+    "armhf"
+    "arm64"
+    "hppa"
+    "i686"
+    "m68k"
+    "mips"
+    "mips64"
+    "mips64el"
+    "mips64r6"
+    "mips64r6el"
+    "mipsel"
+    "mipsr6"
+    "mipsr6el"
+    "ppc"
+    "ppc64"
+    "ppc64le"
+    "riscv64"
+    "s390x"
+    "sh4"
+    "sparc64"
+    "x86_64"
+    # Very slow, do last.
+    "all"
+)
 
-# Very slow, do last.
-docker push ahuszagh/cross:all
+for name in "${DOCKERFILES[@]}"; do
+    docker push "ahuszagh/cross:$name"
+done
