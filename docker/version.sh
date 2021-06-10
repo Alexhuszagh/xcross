c218c1efec8c805fc0f5d2d6b03f42258976cd52
Alex Huszagh <ahuszagh@gmail.com>
2021-06-10 14:50:01 -0500

    Updated version for release.

--- docker/version.sh
+++ docker/version.sh
@@ -3,7 +3,7 @@
 
 export VERSION_MAJOR='0'
 export VERSION_MINOR='1'
-export VERSION_PATCH='0'
+export VERSION_PATCH='1'
 export VERSION_BUILD=''
-export VERSION_INFO=('0' '1' '0' '')
-export VERSION='0.1'
+export VERSION_INFO=('0' '1' '1' '')
+export VERSION='0.1.1'
